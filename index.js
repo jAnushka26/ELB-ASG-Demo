b1cdf58f0fe7d8799c7370a68b608ce1013c3cba
jAnushka26 <ANUSHKA.AJIT-JADHAV@CAPGEMINI.COM>
2025-05-21 13:47:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+const http = require('http');
+ 
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello World\n');
+});
+ 
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
